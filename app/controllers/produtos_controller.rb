2dfcb43973490aac5ace59006d60bbd16c3acd66
Renan do Nascimento Sanches <Renan.sanches_123@hotmail.com>
2018-11-02 19:20:29 -0300

    Adicionado o bootstrap na página de criação

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,12 @@
 class ProdutosController < ApplicationController
     def index
-        @produtos_nome = Produto.all.order(:nome).limit 5
+        @produtos_nome = Produto.all.limit 15
         @produtos_preco = Produto.all.order(:preco).limit 2
     end
+
+    def create
+        valores = params.require(:produto).permit(:nome,:descricao,:quantidade,:preco)
+        @produto = Produto.create valores
+    end
+
 end
